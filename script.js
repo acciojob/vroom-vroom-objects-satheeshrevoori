55d12c690c68e00ed72835866e96e67e82cb1c74
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-03-25 18:50:56 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-// Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
